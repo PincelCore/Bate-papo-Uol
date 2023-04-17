efd540c270307d81a8aac37c2bb4d3f8d64f2b88
PincelCore <alves.davi63@gmail.com>
2023-04-17 12:32:13 -0300

    Chat

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 axios.defaults.headers.common['Authorization'] = 'r8kBMRD2eHzX3IlcxbmI31BL';
 const urlParticipants = 'https://mock-api.driven.com.br/api/vm/uol/participants';
 const urlStatus = 'https://mock-api.driven.com.br/api/vm/uol/status';
+const urlMessage = 'https://mock-api.driven.com.br/api/vm/uol/messages';
 
 const user = {
     name: ''
@@ -44,6 +45,63 @@ function keepAlive() {
 }
 
 enterRoom();
+
+function getMessages() {
+    axios.get(urlMessage)
+      .then(response => {
+        const messages = response.data;
+        const messagesList = document.querySelector('.messages');
+  
+        messagesList.innerHTML = '';
+  
+        messages.forEach(message => {
+          const li = document.createElement('li');
+          const span = document.createElement('span');
+          const strong = document.createElement('strong');
+          const messageText = document.createTextNode(message.text);
+  
+          span.appendChild(document.createTextNode(`(${message.time})`));
+          strong.appendChild(document.createTextNode(message.from));
+          li.appendChild(span);
+          li.appendChild(document.createTextNode(' '));
+          li.appendChild(strong);
+          li.appendChild(document.createTextNode(': '));
+          li.appendChild(messageText);
+  
+          messagesList.appendChild(li);
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  setInterval(() => {
+    getMessages();
+  }, 3000);
+  
+const sendButton = document.getElementById("send-button");
+const messageInput = document.getElementById("message-input");
+
+sendButton.addEventListener("click", () => {
+    const message = messageInput.value;
+    messageInput.value = "";
+
+    const data = {
+        from: user.name,
+        to: "Todos",
+        text: message,
+        type: "message",
+    };
+
+    axios.post(urlMessage, data)
+        .then(() => {
+            getMessages();
+        })
+        .catch(() => {
+            window.location.reload();
+        });
+});
 
 
 
